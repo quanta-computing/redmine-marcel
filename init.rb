40865b9870b958fe2033dd9217523e2f6ca72fd5
Matthieu Rosinski <korrigan@slash16.org>
2015-01-16 12:05:39 +0100

    added user views overriding to allow vacation updating to marcel admins

--- init.rb
+++ init.rb
@@ -1,5 +1,11 @@
 require 'redmine'
 
+module Marcel
+  def self.is_admin?(user)
+    gid = Setting.plugin_marcel[:allowed_edit_group_id]
+    return ((not gid.nil?) and user.is_or_belongs_to?(Group.where(id: gid).first))
+  end
+end
 
 Redmine::Plugin.register :marcel do
   name 'Marcel plugin'
@@ -13,4 +19,6 @@ Redmine::Plugin.register :marcel do
   menu :application_menu, :vacations, { controller: 'vacations', action: 'index'}, caption: 'Vacations'
   menu :application_menu, :vacation_types, { controller: 'vacation_types', action: 'index'}, caption: 'Vacation types'
   settings default: {allowed_edit_group_id: nil}, partial: 'settings/marcel_settings'
+
+  User.safe_attributes 'paid_vacation_days', 'recup_days', if: lambda {|user, current_user| Marcel::is_admin?(current_user)}
 end
