db17c5ff6eae531fd4108ad475d095023725e9b1
Matthieu Rosinski <korrigan@slash16.org>
2015-01-23 19:56:50 +0100

    dealing with custom working hours

--- init.rb
+++ init.rb
@@ -11,5 +11,6 @@ Redmine::Plugin.register :marcel do
   menu :top_menu, :vacations, { controller: 'vacations', action: 'index'}, caption: 'Marcel'
   settings default: {allowed_edit_group_id: nil}, partial: 'settings/marcel_settings'
 
-  User.safe_attributes 'paid_vacation_days', 'recup_days', if: lambda {|user, current_user| Marcel::is_admin?(current_user)}
+  User.safe_attributes 'paid_vacation_days', 'recup_days', 'time_remind_level', if: lambda {|user, current_user| Marcel::is_admin?(current_user)}
+  WorkingHours::Config.working_hours = Marcel::WORKING_HOURS
 end
